5a2aba819c800b414b50096babe1f452ef594176
daniel911texas <dsingletary@gmail.com>
2017-02-04 22:16:40 -0600

    Added TotalSales project files

---
+++ TotalSales/Readme.txt
@@ -0,0 +1,17 @@
+Technical Requirements:
+
+Prompt the user to enter the salesperson’s name, and store this name in a variable.
+
+Prompt the user to enter an item number and a quantity sold of that item.  Store these two entries in two separate variables called “intItem” and “intQuantity”.
+
+Using a DO-WHILE control structure, loop until the user enters -1 for the item number.
+
+Calculate the amount of sales for an item and store the result in a variable called dblItemSales.
+
+After the user enters an item number and a quantity, print to the screen:   the salesperson’s name “has sold “ [intQuantity] “of item # [intItem].
+
+Accumulate the total sales in a variable called dblTotalSales by using a SWITCH statement to select the correct value to be multiplied by the quantity sold and adding this result to a running total, which is stored in the variable dblTotalSales.
+
+If the user enters a number other than 1, 2, 3, or 4, display the message “Invalid Entry” and re-prompt the user to enter an Item Number.  Make sure you do not perform any calculations or prompt the user to enter a quantity if the item number is incorrect.
+
+After accumulating the total sales for an employee (that is, after the user has entered a -1 to quit), print to the screen [Salesperson’s name] sold [number of items] of item # [ item number].
---
+++ TotalSales/TotalSales/Program.cs
@@ -0,0 +1,106 @@
+﻿// TotalSales
+// Daniel Singletary
+// Asks user for a salesperson's name and allows item sales to be added resulting in app calculating total sales
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalSales
+{
+    class TotalSales
+    {
+        static void Main(string[] args)
+        {
+            int intItem = 0;            // declare intItem
+            int intQuantity;        // declare intQuantity
+            double dblItemSales = 0.00;    // declare dblItemSales
+            double dblTotalSales = 0.00;   // declare dblTotalSales
+            double item1 = 239.99;           // placeholder for item 1
+            double item2 = 129.75;           // placeholder for item 2
+            double item3 = 99.95;           // placeholder for item 3
+            double item4 = 350.89;           // placeholder for item 4
+            string salesName;       // declare salespersons name
+            bool validItem;         // checks for valid item number
+
+            Console.Write("Enter a salesperson's name: ");      // Prompt user for salesperson name
+            salesName = Console.ReadLine();
+
+
+            while (intItem != -1)
+            {
+
+
+                do
+                {
+                    Console.Write("Enter an item number between 1 and 4 or -1 to quit: ");
+                    intItem = Convert.ToInt32(Console.ReadLine());
+
+                    if (intItem == -1)
+                    {
+                        validItem = true;
+                    }
+                    else if (intItem < 1)
+                    {
+                        validItem = false;
+                        Console.WriteLine("Invalid Entry");
+                    }
+                    else if (intItem > 4)
+                    {
+                        validItem = false;
+                        Console.WriteLine("Invalid Entry");
+                    }
+                    else
+                    {
+                        validItem = true;
+                    }
+
+
+                } while (validItem == false);
+                if (intItem != -1)
+                {
+
+                    Console.Write("Enter the quantity sold: ");
+                    intQuantity = Convert.ToInt32(Console.ReadLine());
+
+                    switch (intItem)
+                    {
+                        case 1:
+                            dblItemSales = intQuantity * item1;
+                            dblTotalSales = dblTotalSales + dblItemSales;
+                            break;
+                        case 2:
+                            dblItemSales = intQuantity * item2;
+                            dblTotalSales = dblTotalSales + dblItemSales;
+                            break;
+                        case 3:
+                            dblItemSales = intQuantity * item3;
+                            dblTotalSales = dblTotalSales + dblItemSales;
+                            break;
+                        case 4:
+                            dblItemSales = intQuantity * item4;
+                            dblTotalSales = dblTotalSales + dblItemSales;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    Console.WriteLine("Salesperson {0} sold {1} of item #{2} at ${3}", salesName, intQuantity, intItem, dblItemSales);  // Displays item sales
+                    Console.WriteLine();
+                }
+                else if (intItem == -1)
+                {
+                    Console.WriteLine("Salesperson {0} sold a total of ${1}", salesName, dblTotalSales);  // Displays item sales
+                    Console.WriteLine();
+                }
+            }
+
+
+            Console.WriteLine("Press any key to continue...");      // Allows user to see program run before window closes
+            Console.ReadLine();
+
+        }
+    }
+}
